7bba924ce24730894b95f1ce82b688bce9380283
Crow18 <ligumacarl@gmail.com>
2019-12-19 02:47:54 +0200

    gitPush update

--- gitPush.sh
+++ gitPush.sh
@@ -3,4 +3,4 @@
 lastarg=${@:$#}
 beforelast=${@:1:$(($#-1))}
 
-git fetch && git add $beforelast && git commit -m "$last" && git push
+git fetch && git add $beforelast && git commit -m "$lastarg" && git push
