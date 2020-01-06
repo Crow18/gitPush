1a14c6ea7003a89af8e94ab60e4965f5d47f27e7
Carl LIGUMA <cliguma@c3r3s1.wethinkcode.co.za>
2020-01-06 07:22:08 +0200

    new

--- gitPush.sh
+++ gitPush.sh
@@ -5,22 +5,21 @@ beforelast=${@:1:$(($#-1))}
 
 if [ -z $1 ]
 then
-	echo "please add arguments to push, pull or commit"
+	echo "please add arguments to push, pull and commit"
 else
 	read -p "enter Push, Pull or Fetch : " prompt
 	shopt -s nocasematch
-	echo $prompt
-#	if [ "$prompt" != "push" ] | [ "$prompt" != "fetch" ] | [ "$prompt" != "pull" ]
-#	then
-#		echo "command not found!: enter fetch, pull or push"
 	if [ "$prompt" == "fetch" ]
 	then
 		git fetch
 	elif [ "$prompt" == "push" ]
 	then
 		git add $beforelast && git commit -m "$lastarg" && git push
-	else
+	elif [ "$prompt" == "pull" ]
+	then
 		git pull
+	else
+		echo "command not found! : enter 'push', 'pull' or 'fetch'"
 	fi
 fi
 
