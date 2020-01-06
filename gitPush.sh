13a7f1967d4e50ada3f88521aadb2c4c319ef7e1
Carl LIGUMA <cliguma@c3r3s1.wethinkcode.co.za>
2020-01-06 07:18:46 +0200

    updated

--- gitPush.sh
+++ gitPush.sh
@@ -3,4 +3,25 @@
 lastarg=${@:$#}
 beforelast=${@:1:$(($#-1))}
 
-git fetch && git add $beforelast && git commit -m "$lastarg" && git push
+if [ -z $1 ]
+then
+	echo "please add arguments to push, pull or commit"
+else
+	read -p "enter Push, Pull or Fetch : " prompt
+	shopt -s nocasematch
+	echo $prompt
+#	if [ "$prompt" != "push" ] | [ "$prompt" != "fetch" ] | [ "$prompt" != "pull" ]
+#	then
+#		echo "command not found!: enter fetch, pull or push"
+	if [ "$prompt" == "fetch" ]
+	then
+		git fetch
+	elif [ "$prompt" == "push" ]
+	then
+		git add $beforelast && git commit -m "$lastarg" && git push
+	else
+		git pull
+	fi
+fi
+
+#git fetch && git add $beforelast && git commit -m "$lastarg" && git push
